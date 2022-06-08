0a0d3fbff80f4324157316109e08f036f37b434d
asemin08 <semin.allan@gmail.com>
2022-06-08 16:12:16 +0200

    Ajout class pour les suite de test

---
+++ src/test/java/eu/esnup/gestioncomptetest/services/SuiteTest.java
@@ -0,0 +1,9 @@
+package eu.esnup.gestioncomptetest.services;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@SuiteDisplayName("Test Suite pour gestion Compte")
+@SelectClasses({CarteServiceTest.class})
+public class SuiteTest {
+}
